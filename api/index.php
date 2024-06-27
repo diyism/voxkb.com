43064eb416b5aa3c777e176ac278b87d7e6a97b8
Malcolm Ke Win <kexianbin@diyism.com>
2024-06-27 20:35:33 +0800

    Create index.php

---
+++ api/index.php
@@ -0,0 +1,16 @@
+<script src="https://cdn.jsdelivr.net/npm/onnxruntime-web@1.14.0/dist/ort.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@ricky0123/vad-web@0.0.7/dist/bundle.min.js"></script>
+<script>
+  async function main() {
+    const myvad = await vad.MicVAD.new({
+      onSpeechStart: () => {
+        console.log("Speech start detected")
+      },
+      onSpeechEnd: (audio) => {
+        // do something with `audio` (Float32Array of audio samples at sample rate 16000)...
+      }
+    })
+    myvad.start()
+  }
+  main()
+</script>
